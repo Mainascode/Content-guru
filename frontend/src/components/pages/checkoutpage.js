e13ad958572138b3912e08798a388383d6eacead
Maina <smurkcarter855@gmail.com>
2025-07-02 08:25:37 +0300

    changes

--- frontend/src/components/pages/checkoutpage.js
+++ frontend/src/components/pages/checkoutpage.js
@@ -22,7 +22,7 @@ export default function CheckoutPage() {
   const handlePayPalSuccess = async (details, data) => {
     setLoading(true);
     try {
-      const res = await fetch("https://content-guru-gpls.onrender.com/paypal/purchase-book", {
+      const res = await fetch("https://content-guru-gpls.onrender.com/purchase-book", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
