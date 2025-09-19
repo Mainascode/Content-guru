3e09b5c2f864fda0ddcd68e5966626462f418415
Maina <smurkcarter855@gmail.com>
2025-09-19 15:19:33 +0300

    origin

--- backend/server/models.py
+++ backend/server/models.py
@@ -175,12 +175,9 @@ class CoursePurchase(db.Model):
     
     
 class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-
+    __tablename__ = 'blogs'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
-
-    author = db.relationship("User", backref="blog_posts")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)  # optional
